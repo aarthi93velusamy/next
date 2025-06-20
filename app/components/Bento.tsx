8dbac6104e059ee5070b84dd73165cb789699969
aarthi93velusamy <aarthi93.velusamy@gmail.com>
2025-06-20 13:52:46 +0530

    code coreected

--- app/components/Bento.tsx
+++ app/components/Bento.tsx
@@ -6,7 +6,7 @@ export default function Bento() {
       <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
         <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
-          Everything you need to deploy your ap
+          Everything you need to deploy your app
         </p>
         <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
           <div className="relative lg:row-span-2">
